be63e9e6bb378e97643a0800d8cd29a64b5bfec5
Daniel Bartet <dbartet@progroupx.com>
2025-06-07 15:49:25 -0300

    fix: ajustada respuesta de CORS para devolver el origen correcto

--- src/app.js
+++ src/app.js
@@ -10,11 +10,7 @@ const app = express();
 app.use(cors({
   origin: function(origin, callback) {
     const allowedOrigins = [process.env.FRONTEND_URL || "http://localhost:8080", "https://jobsandjobs.com"];
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
+    callback(null, origin && allowedOrigins.indexOf(origin) !== -1 ? origin : allowedOrigins[0]);
   },
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true
