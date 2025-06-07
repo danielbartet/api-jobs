db5685451cfd316a59ed720b5fd2f9297ee28dda
Daniel Bartet <dbartet@progroupx.com>
2025-06-07 17:12:42 -0300

    fix: simplificada configuración de Traefik y CORS

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: function(origin, callback) {
+  origin: function (origin, callback) {
     const allowedOrigins = [process.env.FRONTEND_URL || "http://localhost:8080", "https://jobsandjobs.com"];
     callback(null, origin && allowedOrigins.indexOf(origin) !== -1 ? origin : allowedOrigins[0]);
   },
